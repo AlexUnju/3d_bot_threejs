ca2e17afb1533fdb59fe83f574995435685bbd14
Alexxe <Alexxe@fi.unju.edu.ar>
2023-11-02 14:12:36 -0300

    commit-1:crear app + config

---
+++ components/TextToSpeech.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TextToSpeech = () => {
+  return (
+    <div>
+        <form>
+            <input className='bg-transparent w-[510px] border border-[#C441FF] / 8 ' type="text" />
+            <button>Preguntar</button>
+        </form>
+    </div>
+  )
+}
